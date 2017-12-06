b04b5cb945702aaebd19a39903fbe198646616a1
Filippos Georgiou <fgeorgiou@amcstudent.edu.gr>
2017-12-07 00:52:14 +0200

    Day's final commit: 1) Added full functionality to services pill in admin interface 2)Paved the way for the rest of the pills 3)Initiated the my account page cleanup 4)create the path to the same page. All project files are now connected properly 5)more html edits

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -8,7 +8,17 @@ class AddServiceForm(FlaskForm):
   	cost = StringField("Cost:", [InputRequired()])
   	submit = SubmitField("Submit")
 
-#This is the form that will service removals
-class RemoveServiceForm(FlaskForm):
-  	name = StringField("Service Name:", [InputRequired()])
-  	submit = SubmitField("Submit")
+#This is the form that will handle user registrations. TODO implement an edit system for admins
+# class EditForm(FlaskForm):
+# 	first_name = StringField('First Name:', validators=[InputRequired(), Regexp('^[a-zA-Z ]*$', message='Only letters and white space allowed')])
+# 	last_name = StringField('Last Name:', validators=[InputRequired(), Regexp('^[a-zA-Z ]*$', message='Only letters and white space allowed')])
+# 	birth_date = DateField('Date of Birth:', format='%m/%d/%Y')
+# 	email = StringField("Email", validators=[InputRequired(), Email()])
+# 	telephone = StringField("Telephone:", [InputRequired(), Regexp('^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$', message='Only 10-digit phone numbers and prefixes are accepted.')])
+# 	password = PasswordField('Password:', validators=[InputRequired(),EqualTo('confirm_password'), Regexp('^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$', message='Must contain 1 capital letter, 1 number & at least 8 characters')])
+# 	confirm_password = PasswordField('Confirm Password:')
+# 	submit = SubmitField("Register")
+
+# 	def validate_email(self, field):
+# 		if User.query.filter_by(email=field.data).first():
+# 		    raise ValidationError('Email is already in use.')
--- app/admin/views.py
+++ app/admin/views.py
@@ -3,7 +3,7 @@ from flask_login import current_user, login_required
 from datetime import datetime
 
 from . import admin
-from .forms import AddServiceForm, RemoveServiceForm
+from .forms import AddServiceForm
 from .. import db
 from ..models import *
 
@@ -19,40 +19,79 @@ def check_admin():
 	if not current_user.is_admin:
 		abort(403)
 
-@admin.route('/services', methods=['GET','POST'])
-def services():
-	"""
-	Admin Interface - Services Function
-	"""
+@admin.route('/add_service', methods=['GET','POST'])
+@login_required
+def add_service():
+    #Create Services
 	check_admin()
 
 	#Instantiating forms
 	add_service_form = AddServiceForm()
-	remove_service_form = RemoveServiceForm()
 
 	if request.method == 'POST':
 		if add_service_form.validate_on_submit():
-			service = Service(name=add_service_form.name.data.capitalize(),
-							cost=add_service_form.cost.data.capitalize())
+			service_to_add = Service(name=add_service_form.name.data.capitalize(),
+							cost=add_service_form.cost.data)
 			try:
 				# add service to the database
-				db.session.add(service)
+				db.session.add(service_to_add)
 				db.session.commit()
 				flash('You have successfully added a new service.', 'info')
 			except:
 				# in case service name already exists
 				flash('Error: service name already exists.', 'error')
 
-		# redirect to admin interface page
-		return redirect(url_for('admin.services'))
+			return redirect(url_for('admin.admin_interface'))
+
+		#remove service form logic
+		if remove_service_form.validate_on_submit():
+			service_to_delete = Service.query.get(id)
+			db.session.delete(service_to_delete)
+			db.session.commit()
+			flash('You have successfully deleted a service.', 'info')
+
+			return redirect(url_for('admin.admin_interface'))
+
+	return render_template('admin/admin_interface.html',
+								add_service_form=add_service_form)
+
+
+@admin.route('/delete_service/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_service(id):
+    #Delete Services
+    check_admin()
+
+    service_to_delete = Service.query.get_or_404(id)
+    db.session.delete(service_to_delete)
+    db.session.commit()
+    flash('You have successfully deleted a service.', 'info')
+
+    # redirect to the departments page
+    return redirect(url_for('admin.admin_interface'))
+
+    return render_template('admin/admin_interface.html')
+
+@admin.route('/admin_interface', methods=['GET','POST'])
+@login_required
+def admin_interface():
+	"""
+	Admin Interface - Users View
+	"""
+	check_admin()
+
+	#Query the database for the required info
+	users_result = User.query.all()
+	services_result = Service.query.all()
 
 	return render_template('admin/admin_interface.html',
 								title="Admin_Interface",
 								company_name=company_name,
 								users=users_result,
 								services=services_result,
-								add_service_form=add_service_form,
-								remove_service_form=remove_service_form)
+								add_service_form=AddServiceForm())
+
+
 
 # @admin.route('/appointments', methods=['GET','POST'])
 # @login_required
@@ -85,26 +124,4 @@ def services():
 # 								title="Admin_Interface",
 # 								company_name=company_name,
 # 								users=users)
-
-
-
-@admin.route('/admin_interface', methods=['GET','POST'])
-@login_required
-def admin_interface():
-	"""
-	Admin Interface - Users View
-	"""
-	check_admin()
-
-	#Query the database for the required info
-	users_result = User.query.all()
-	services_result = Service.query.all()
-
-	return render_template('admin/admin_interface.html',
-								title="Admin_Interface",
-								company_name=company_name,
-								users=users_result,
-								services=services_result,
-								add_service_form=AddServiceForm(),
-								remove_service_form=RemoveServiceForm())
 
